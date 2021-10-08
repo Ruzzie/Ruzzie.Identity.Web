31648aeb8e91470f8d425d74f10d62e3ea0c9682
Linestara <dave_van_der_horst@hotmail.com>
2021-10-08 12:18:54 +0200

    [fix] Upgraded restsharp nuget package to latest version, to be same as other projects. Else we get an runtime error. Added some tests to check this.

--- src/Ruzzie.Identity.Web/Email.fs
+++ src/Ruzzie.Identity.Web/Email.fs
@@ -55,36 +55,38 @@ module Email =
 
         [<Literal>]
         let baseUrl = "https://api.eu.mailgun.net/v3"
+                                                
+        let createMailGunSendEmailRestRequest (emailRequest : SendEmailRequest) (mailDomain : string)  =
+            let request = RestRequest("{domain}/messages", Method.POST)
+            request.AddParameter(Parameter("domain", mailDomain, ParameterType.UrlSegment)) |> ignore
+            request.AddParameter("from", (StringNonEmpty.value emailRequest.From)) |> ignore
+            request.AddParameter("to", (EmailAddressValue.value emailRequest.To)) |> ignore
+            request.AddParameter("subject", (StringNonEmpty.value emailRequest.Subject)) |> ignore
+            request.AddParameter("text", (StringNonEmpty.value emailRequest.TextContent)) |> ignore
 
-        let private client =  RestSharp.RestClient(baseUrl) :> IRestClient
+            if not (System.String.IsNullOrWhiteSpace(emailRequest.HtmlContent)) then
+                request.AddParameter("html", (emailRequest.HtmlContent)) |> ignore
+            else
+                ()
 
-        let mailgunSendMail apiKey mailDomain emailRequest =
+            match emailRequest.ReplyTo with
+            |Some replyTo -> request.AddParameter("h:Reply-To", (EmailAddressValue.value replyTo)) |> ignore
+            |None -> ()
+            
+            request
+                                                                  
+        let mailgunSendMail apiKey mailDomain (emailRequest:SendEmailRequest) =
+            
+            let restClient = RestClient(baseUrl) :> IRestClient
+            restClient.Authenticator <- HttpBasicAuthenticator("api", apiKey)
+           
+            let request = createMailGunSendEmailRestRequest emailRequest mailDomain
 
-            let sendEmail emailRequest =
-                client.Authenticator <- HttpBasicAuthenticator("api", apiKey)
+            let response = restClient.Execute(request)
 
-                let request = RestRequest("{domain}/messages", Method.POST)
-                request.AddParameter(Parameter("domain", mailDomain, ParameterType.UrlSegment)) |> ignore
-                request.AddParameter("from", (StringNonEmpty.value emailRequest.From)) |> ignore
-                request.AddParameter("to", (EmailAddressValue.value emailRequest.To)) |> ignore
-                request.AddParameter("subject", (StringNonEmpty.value emailRequest.Subject)) |> ignore
-                request.AddParameter("text", (StringNonEmpty.value emailRequest.TextContent)) |> ignore
+            if response.IsSuccessful then
+                Ok(response.StatusCode)
+            else
+                Error(response.StatusCode, response.ErrorMessage + ";Content: [" + response.Content + "]")
 
-                if not (System.String.IsNullOrWhiteSpace(emailRequest.HtmlContent)) then
-                    request.AddParameter("html", (emailRequest.HtmlContent)) |> ignore
-                else
-                    ()
-
-                match emailRequest.ReplyTo with
-                |Some replyTo -> request.AddParameter("h:Reply-To", (EmailAddressValue.value replyTo)) |> ignore
-                |None -> ()
-
-                let response = client.Execute(request)
-
-                if response.IsSuccessful then
-                    Ok(response.StatusCode)
-                else
-                    Error(response.StatusCode, response.ErrorMessage + ";Content: [" + response.Content + "]")
-
-
-            (sendEmail emailRequest)
+           
