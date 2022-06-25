b30ddc612183dc06445d70ea0962bd8eb67d160b
Ruzzie <dorus.verhoeckx@gmail.com>
2022-06-25 06:20:28 +0200

    adds extra overload to typed cache, some minor cleanup

--- src/Ruzzie.Extensions.UnitTests/Caching/DistributedTypedCacheTests.cs
+++ src/Ruzzie.Extensions.UnitTests/Caching/DistributedTypedCacheTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using FluentAssertions;
 using FsCheck;
@@ -9,219 +10,239 @@ using Microsoft.Extensions.Options;
 using NUnit.Framework;
 using Ruzzie.Extensions.Caching;
 
-namespace Ruzzie.Extensions.UnitTests.Caching
+namespace Ruzzie.Extensions.UnitTests.Caching;
+
+[TestFixture]
+public class DistributedTypedCacheTests
 {
-    [TestFixture]
-    public class DistributedTypedCacheTests
+    private DistributedTypedCache<MemoryDistributedCache> _cache = null!;
+
+
+    [SetUp]
+    public void Setup()
     {
-        private DistributedTypedCache _cache = null!;
+        var optionsFactory =
+            new OptionsFactory<MemoryDistributedCacheOptions>(new List<IConfigureOptions<
+                                                                  MemoryDistributedCacheOptions>>()
+                                                            , new List<IPostConfigureOptions<
+                                                                  MemoryDistributedCacheOptions>>());
+        var options                 = new OptionsManager<MemoryDistributedCacheOptions>(optionsFactory);
+        var distributedCacheForTest = new MemoryDistributedCache(options);
 
-
-        [SetUp]
-        public void Setup()
-        {
-            var distributedCacheForTest =
-                new MemoryDistributedCache(new OptionsManager<MemoryDistributedCacheOptions>(
-                                            new OptionsFactory<MemoryDistributedCacheOptions>(
-                                             new List<IConfigureOptions<MemoryDistributedCacheOptions>>(),
-                                             new List<IPostConfigureOptions<MemoryDistributedCacheOptions>
-                                             >())));
-
-            _cache = new DistributedTypedCache(distributedCacheForTest,
-                                               nameof(DistributedTypedCacheTests),
-                                               new DistributedCacheEntryOptions
-                                               {
-                                                   AbsoluteExpirationRelativeToNow =
-                                                       TimeSpan.FromSeconds(2)
-                                               });
-        }
-
-        [FsCheck.NUnit.Property]
-        public void GetOrAddPropertyTest(NonEmptyString key)
-        {
-            //Arrange
-            var entryToAdd =
-                new DataToCache("Org", "GetOrAddPropertyTest", DateTimeOffset.UtcNow, DateTimeOffset.UtcNow);
-
-            //Act
-            var entryFromCache = _cache.GetOrAdd($"{nameof(GetOrAddPropertyTest)}{key.Get}", _ => entryToAdd);
-
-            //Assert
-            entryFromCache.Should()
-                          .BeEquivalentTo(entryToAdd
-                                        , options => options.Excluding(x => x.LastModifiedAt)
-                                                            .Excluding(x => x.CreatedAt));
-        }
-
-
-        [FsCheck.NUnit.Property]
-        public void SetGetPropertyTest(NonEmptyString key)
-        {
-            //Arrange
-            var entryToAdd = new DataToCache("Org", "SetGetPropertyTest", DateTimeOffset.UtcNow,
-                                             DateTimeOffset.UtcNow);
-            var cacheKey = $"{nameof(SetGetPropertyTest)}{key.Get}";
-
-            //Act
-            _cache.Set(cacheKey, entryToAdd);
-            var entryFromCache = _cache.Get<DataToCache>(cacheKey);
-
-            //Assert
-            // ignore the last modification datetime, since the same key could be added twice, and thus the timestamp can differ.
-            entryFromCache.Should()
-                          .BeEquivalentTo(entryToAdd, options => options.Excluding(x => x.LastModifiedAt));
-        }
-
-        [FsCheck.NUnit.Property]
-        public void SetGetAsyncPropertyTest(NonEmptyString key)
-        {
-            //Arrange
-            var entryToAdd = new DataToCache("Org", "SetGetAsyncPropertyTest", DateTimeOffset.UtcNow,
-                                             DateTimeOffset.UtcNow);
-
-            var cacheKey = $"{nameof(SetGetAsyncPropertyTest)}{key.Get}";
-
-            //Act
-            _cache.SetAsync(cacheKey, entryToAdd).GetAwaiter().GetResult();
-            var entryFromCache = _cache.GetAsync<DataToCache>(cacheKey).GetAwaiter().GetResult();
-
-            //Assert
-            entryFromCache.Should()
-                          .BeEquivalentTo(entryToAdd, options => options.Excluding(x => x.LastModifiedAt));
-        }
-
-        [Test]
-        public void GetOrAddAddsSuccess()
-        {
-            //Arrange
-            var entryToAdd =
-                new DataToCache("Org", "GetOrAddAddsSuccess", DateTimeOffset.UtcNow, DateTimeOffset.UtcNow);
-            //Act
-            var entryFromCache = _cache.GetOrAdd(nameof(GetOrAddAddsSuccess), _ => entryToAdd);
-
-            //Assert
-            entryFromCache.Should().BeEquivalentTo(entryToAdd);
-        }
-
-        [Test]
-        public async Task GetOrAddAddsAsyncSuccess()
-        {
-            //Arrange
-            var entryToAdd = new DataToCache("Org", "GetOrAddAddsAsyncSuccess", DateTimeOffset.UtcNow,
-                                             DateTimeOffset.UtcNow);
-
-            //Act
-            var entryFromCache =
-                await _cache.GetOrAddAsync(nameof(GetOrAddAddsAsyncSuccess), _ => Task.FromResult(entryToAdd));
-
-            //Assert
-            entryFromCache.Should().BeEquivalentTo(entryToAdd);
-        }
-
-        [Test]
-        public void GetOrAddGetsSuccess()
-        {
-            //Arrange
-            int timesCalled = 0;
-            var entryToAdd =
-                new DataToCache("Org", "GetOrAddGetsSuccess", DateTimeOffset.UtcNow, DateTimeOffset.UtcNow);
-
-            _cache.GetOrAdd(nameof(GetOrAddGetsSuccess),
-                            _ =>
-                            {
-                                timesCalled++;
-                                return entryToAdd;
-                            });
-
-            //Act
-            var entryFromCache = _cache.GetOrAdd(nameof(GetOrAddGetsSuccess),
-                                                 _ =>
-                                                 {
-                                                     timesCalled++;
-                                                     return entryToAdd;
-                                                 });
-
-
-            //Assert
-            entryFromCache.Should().BeEquivalentTo(entryToAdd);
-            timesCalled.Should().Be(1);
-        }
-
-        [Test]
-        public void GetAndRemoveSuccess()
-        {
-            //Arrange
-            var entryFromCache =
-                _cache.GetOrAdd(nameof(GetAndRemoveSuccess),
-                                _ => new DataToCache("Org", "TestUser", DateTimeOffset.UtcNow, DateTimeOffset.UtcNow));
-
-            //precondition check
-            _cache.Get<DataToCache>(nameof(GetAndRemoveSuccess)).Should().BeEquivalentTo(entryFromCache);
-
-            //Act
-            _cache.Remove(nameof(GetAndRemoveSuccess));
-
-            //Assert
-            _cache.Get<DataToCache>(nameof(GetAndRemoveSuccess)).Should().BeNull();
-        }
-
-        [FsCheck.NUnit.Property]
-        public void GetAndRemovePropertyTests(NonEmptyString key)
-        {
-            //Arrange
-            var entryFromCache =
-                _cache.GetOrAdd(key.Get,
-                                _ => new DataToCache("Org", "TestUser", DateTimeOffset.UtcNow, DateTimeOffset.UtcNow));
-
-            //precondition check
-            _cache.Get<DataToCache>(key.Get).Should().BeEquivalentTo(entryFromCache);
-
-            //Act
-            _cache.Remove(key.Get);
-
-            //Assert
-            _cache.Get<DataToCache>(key.Get).Should().BeNull();
-        }
-
-        [FsCheck.NUnit.Property]
-        public void RemoveNonExistentKeysNoErrors(NonEmptyString key)
-        {
-            //Arrange & Act
-            _cache.Remove(key.Get);
-        }
-
-        [Test]
-        public async Task GetAndRemoveAsyncSuccess()
-        {
-            //Arrange
-            var entryFromCache =
-                _cache.GetOrAdd(nameof(GetAndRemoveSuccess),
-                                _ => new DataToCache("Org", "TestUser", DateTimeOffset.UtcNow, DateTimeOffset.UtcNow));
-
-            //precondition check
-            (await _cache.GetAsync<DataToCache>(nameof(GetAndRemoveSuccess))).Should().BeEquivalentTo(entryFromCache);
-
-            //Act
-            await _cache.RemoveAsync(nameof(GetAndRemoveSuccess));
-
-            //Assert
-            (await _cache.GetAsync<DataToCache>(nameof(GetAndRemoveSuccess))).Should().BeNull();
-        }
+        _cache = new DistributedTypedCache<MemoryDistributedCache>(distributedCacheForTest
+                                                                 , nameof(DistributedTypedCacheTests)
+                                                                 , new DistributedCacheEntryOptions
+                                                                   {
+                                                                       AbsoluteExpirationRelativeToNow =
+                                                                           TimeSpan.FromSeconds(2)
+                                                                   });
     }
 
-    public class DataToCache
+    [FsCheck.NUnit.Property]
+    public void GetOrAddPropertyTest(NonEmptyString key)
     {
-        public string         Id             { get; }
-        public string         Name           { get; }
-        public DateTimeOffset CreatedAt      { get; }
-        public DateTimeOffset LastModifiedAt { get; }
+        //Arrange
+        var entryToAdd =
+            new DataToCache("Org", "GetOrAddPropertyTest", DateTimeOffset.UtcNow, DateTimeOffset.UtcNow);
 
-        public DataToCache(string id, string name, DateTimeOffset createdAt, DateTimeOffset lastModifiedAt)
+        //Act
+        var entryFromCache = _cache.GetOrAdd($"{nameof(GetOrAddPropertyTest)}{key.Get}", _ => entryToAdd);
+
+        //Assert
+        entryFromCache.Should()
+                      .BeEquivalentTo(entryToAdd
+                                    , options => options.Excluding(x => x.LastModifiedAt)
+                                                        .Excluding(x => x.CreatedAt));
+    }
+
+
+    [FsCheck.NUnit.Property]
+    public void SetGetPropertyTest(NonEmptyString key)
+    {
+        //Arrange
+        var entryToAdd = new DataToCache("Org", "SetGetPropertyTest", DateTimeOffset.UtcNow, DateTimeOffset.UtcNow);
+        var cacheKey   = $"{nameof(SetGetPropertyTest)}{key.Get}";
+
+        //Act
+        _cache.Set(cacheKey, entryToAdd);
+        var entryFromCache = _cache.Get<DataToCache>(cacheKey);
+
+        //Assert
+        // ignore the last modification datetime, since the same key could be added twice, and thus the timestamp can differ.
+        entryFromCache.Should()
+                      .BeEquivalentTo(entryToAdd, options => options.Excluding(x => x.LastModifiedAt));
+    }
+
+    [FsCheck.NUnit.Property]
+    public void SetGetAsyncPropertyTest(NonEmptyString key)
+    {
+        //Arrange
+        var entryToAdd = new DataToCache("Org"
+                                       , "SetGetAsyncPropertyTest"
+                                       , DateTimeOffset.UtcNow
+                                       , DateTimeOffset.UtcNow);
+
+        var cacheKey = $"{nameof(SetGetAsyncPropertyTest)}{key.Get}";
+
+        //Act
+        _cache.SetAsync(cacheKey, entryToAdd).GetAwaiter().GetResult();
+        var entryFromCache = _cache.GetAsync<DataToCache>(cacheKey).GetAwaiter().GetResult();
+
+        //Assert
+        entryFromCache.Should()
+                      .BeEquivalentTo(entryToAdd, options => options.Excluding(x => x.LastModifiedAt));
+    }
+
+    [Test]
+    public void GetOrAddAddsSuccess()
+    {
+        //Arrange
+        var entryToAdd =
+            new DataToCache("Org", "GetOrAddAddsSuccess", DateTimeOffset.UtcNow, DateTimeOffset.UtcNow);
+        //Act
+        var entryFromCache = _cache.GetOrAdd(nameof(GetOrAddAddsSuccess), _ => entryToAdd);
+
+        //Assert
+        entryFromCache.Should().BeEquivalentTo(entryToAdd);
+    }
+
+    [Test]
+    public unsafe void GetOrAddDelegatePtr()
+    {
+        //Act
+        var entryFromCache = _cache.GetOrAdd(nameof(GetOrAddAddsSuccess), &Add);
+
+        //Assert
+        entryFromCache.Should().BeEquivalentTo(Add(""));
+
+        static DataToCache Add(string key)
         {
-            Id             = id;
-            Name           = name;
-            CreatedAt      = createdAt;
-            LastModifiedAt = lastModifiedAt;
+            return new DataToCache("Org", "GetOrAddDelegatePtr", DateTimeOffset.UtcNow, DateTimeOffset.UtcNow);
         }
+            
+    }
+
+    [Test]
+    public async Task GetOrAddAddsAsyncSuccess()
+    {
+        //Arrange
+        var entryToAdd = new DataToCache("Org"
+                                       , "GetOrAddAddsAsyncSuccess"
+                                       , DateTimeOffset.UtcNow
+                                       , DateTimeOffset.UtcNow);
+
+        //Act
+        var entryFromCache =
+            await _cache.GetOrAddAsync(nameof(GetOrAddAddsAsyncSuccess), _ => Task.FromResult(entryToAdd));
+
+        //Assert
+        entryFromCache.Should().BeEquivalentTo(entryToAdd);
+    }
+
+    [Test]
+    public void GetOrAddGetsSuccess()
+    {
+        //Arrange
+        int timesCalled = 0;
+        var entryToAdd =
+            new DataToCache("Org", "GetOrAddGetsSuccess", DateTimeOffset.UtcNow, DateTimeOffset.UtcNow);
+
+        _cache.GetOrAdd(nameof(GetOrAddGetsSuccess)
+                      , _ =>
+                        {
+                            timesCalled++;
+                            return entryToAdd;
+                        });
+
+        //Act
+        var entryFromCache = _cache.GetOrAdd(nameof(GetOrAddGetsSuccess)
+                                           , _ =>
+                                             {
+                                                 timesCalled++;
+                                                 return entryToAdd;
+                                             });
+
+
+        //Assert
+        entryFromCache.Should().BeEquivalentTo(entryToAdd);
+        timesCalled.Should().Be(1);
+    }
+
+    [Test]
+    public void GetAndRemoveSuccess()
+    {
+        //Arrange
+        var entryFromCache =
+            _cache.GetOrAdd(nameof(GetAndRemoveSuccess)
+                          , _ => new DataToCache("Org", "TestUser", DateTimeOffset.UtcNow, DateTimeOffset.UtcNow));
+
+        //precondition check
+        _cache.Get<DataToCache>(nameof(GetAndRemoveSuccess)).Should().BeEquivalentTo(entryFromCache);
+
+        //Act
+        _cache.Remove(nameof(GetAndRemoveSuccess));
+
+        //Assert
+        _cache.Get<DataToCache>(nameof(GetAndRemoveSuccess)).Should().BeNull();
+    }
+
+    [FsCheck.NUnit.Property]
+    public void GetAndRemovePropertyTests(NonEmptyString key)
+    {
+        //Arrange
+        var entryFromCache =
+            _cache.GetOrAdd(key.Get
+                          , _ => new DataToCache("Org", "TestUser", DateTimeOffset.UtcNow, DateTimeOffset.UtcNow));
+
+        //precondition check
+        _cache.Get<DataToCache>(key.Get).Should().BeEquivalentTo(entryFromCache);
+
+        //Act
+        _cache.Remove(key.Get);
+
+        //Assert
+        _cache.Get<DataToCache>(key.Get).Should().BeNull();
+    }
+
+    [FsCheck.NUnit.Property]
+    public void RemoveNonExistentKeysNoErrors(NonEmptyString key)
+    {
+        //Arrange & Act
+        _cache.Remove(key.Get);
+    }
+
+    [Test]
+    public async Task GetAndRemoveAsyncSuccess()
+    {
+        //Arrange
+        var entryFromCache =
+            _cache.GetOrAdd(nameof(GetAndRemoveSuccess)
+                          , _ => new DataToCache("Org", "TestUser", DateTimeOffset.UtcNow, DateTimeOffset.UtcNow));
+
+        //precondition check
+        (await _cache.GetAsync<DataToCache>(nameof(GetAndRemoveSuccess))).Should().BeEquivalentTo(entryFromCache);
+
+        //Act
+        await _cache.RemoveAsync(nameof(GetAndRemoveSuccess));
+
+        //Assert
+        (await _cache.GetAsync<DataToCache>(nameof(GetAndRemoveSuccess))).Should().BeNull();
+    }
+}
+
+[SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
+public class DataToCache
+{
+    public string         Id             { get; }
+    public string         Name           { get; }
+    public DateTimeOffset CreatedAt      { get; }
+    public DateTimeOffset LastModifiedAt { get; }
+
+    public DataToCache(string id, string name, DateTimeOffset createdAt, DateTimeOffset lastModifiedAt)
+    {
+        Id             = id;
+        Name           = name;
+        CreatedAt      = createdAt;
+        LastModifiedAt = lastModifiedAt;
     }
 }
